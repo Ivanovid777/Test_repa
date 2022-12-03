af1816b90c7146fe9fb23ea2abe812c4b8b417b0
Ivanoid777 <ivanqqqe@gmail.com>
2022-12-03 22:02:52 +0200

    first commit

---
+++ Clock_mymymy.ino
@@ -0,0 +1,48 @@
+uint8_t hour = 0;
+uint8_t min = 0;
+uint8_t sec = 0;
+long msec0 = 0;
+long msec = 0;
+
+String check(uint8_t data)
+{
+  String ss ="";
+  if (data <= 9)
+  {
+    return "0" + String(data);
+  }
+  else 
+    return String(data);
+ 
+}
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  msec = millis();
+  if(msec - msec0 >= 10){
+    msec0 = msec;
+    sec ++;
+  
+    if(sec >= 59){
+      min ++;
+      sec = 0;
+      
+      if(min >= 59){
+        hour ++;
+        min = 0;
+        
+        if(hour >= 23){
+        hour =0;
+    
+        }  
+      }
+    }
+    
+    Serial.println(check(hour) + ":" + check(min) + ":" + check(sec));
+    }
+}
+ 
